a8d2da490eaba9f7dcbfb158e01e3d527f0674af
Romain Dubuc <romaindubuc62@gmail.com>
2021-05-11 20:35:35 +0200

    Add countries field + ajax request to get countries

--- src/js/SignUp.js
+++ src/js/SignUp.js
@@ -1,30 +1,46 @@
 import { Component } from 'react';
 import 'uikit/dist/css/uikit.min.css'
 
-const initState = {
-    firstName: '',
-    lastName: '',
-    birthDate: '',
-    description: '',
-    email: '',
-    username: '',
-    password: '',
-    repassword: ''
-}
-
 export default class SignUp extends Component {
-    constructor(props) {
-        super(props)
-        this.state = initState;
+    state = {
+        user: {
+            firstName: '',
+            lastName: '',
+            country: '',
+            birthDate: '',
+            description: '',
+            email: '',
+            username: '',
+            password: '',
+            repassword: '',
+        },
+        countries: [],
     }
 
-    checkForm(form) {
+    componentDidMount() {
+		this.getCountries();
+	}
+
+    getCountries() {
+        return fetch('http://localhost:8080/api/countries')
+            .then(response => response.json())
+            .then(countries => this.setState({ countries }));
+    }
+
+    checkForm() {
         let isValid = true;
 
         document.querySelectorAll("input").forEach(input => {
             if (!input.value) {
                 isValid = false;
                 this.setValidationInput(input, false);
+            }
+        })
+
+        document.querySelectorAll("select").forEach(select => {
+            if (!select.value) {
+                isValid = false;
+                this.setValidationInput(select, false);
             }
         })
 
@@ -57,15 +73,13 @@ export default class SignUp extends Component {
 
     handleSubmit(event) {
         event.preventDefault();
-        if (!this.checkForm(event.target)) {
+        if (!this.checkForm()) {
             return false;
         }
 
         fetch(`http://localhost:8080/api/users`, {
-            method: 'POST', body: JSON.stringify(this.state)
+            method: 'POST', body: JSON.stringify(this.state.user)
         });
-
-        //this.setState(initState);
     }
 
     setValidationInput(input, isValid) {
@@ -84,48 +98,52 @@ export default class SignUp extends Component {
 
     handleFirstNameChange(event) {
         this.checkEmptyInput(event.target);
-        this.setState({ firstName: event.target.value });
+        this.setState({ user: { ...this.state.user, firstName: event.target.value } });
     }
 
     handleLastNameChange(event) {
         this.checkEmptyInput(event.target);
-        this.setState({ lastName: event.target.value });
+        this.setState({ user: { ...this.state.user, lastName: event.target.value } });
+    }
+
+    handleCountryChange(event) {
+        this.checkEmptyInput(event.target);
+        this.setState({ user: { ...this.state.user, country: event.target.value } });
     }
 
     handleUserNameChange(event) {
         this.checkEmptyInput(event.target);
-        this.setState({ username: event.target.value });
+        this.setState({ user: { ...this.state.user, username: event.target.value } });
     }
 
     handleEmailChange(event) {
         this.checkEmptyInput(event.target);
         this.checkEmail(event.target);
-        this.setState({ email: event.target.value });
+        this.setState({ user: { ...this.state.user, email: event.target.value } });
     }
 
     handleDescriptionChange(event) {
         this.checkEmptyInput(event.target);
-        this.setState({ description: event.target.value });
+        this.setState({ user: { ...this.state.user, description: event.target.value } });
     }
 
     handleBirthDateChange(event) {
         this.checkEmptyInput(event.target);
         this.checkAge(event.target);
-        this.setState({ birthDate: event.target.value });
+        this.setState({ user: { ...this.state.user, birthDate: event.target.value } });
     }
 
     handlePasswordChange(event) {
         this.checkEmptyInput(event.target);
-        this.setState({ password: event.target.value });
+        this.setState({ user: { ...this.state.user, password: event.target.value } });
     }
 
     handleRePasswordChange(event) {
         this.checkEmptyInput(event.target);
-        this.setState({ repassword: event.target.value });
+        this.setState({ user: { ...this.state.user, repassword: event.target.value } });
     }
 
 	render() {
-
 		return (
 			<div className="uk-container uk-container-xsmall">
 				<header>
@@ -133,27 +151,42 @@ export default class SignUp extends Component {
 				</header>
                 <form onSubmit={event =>this.handleSubmit(event)}>
                     <label htmlFor="firstname">Firstname</label>
-                    <input className="uk-input" type="text" id="firstname" onChange={e =>this.handleFirstNameChange(e)} value={this.state.firstName} />
+                    <input className="uk-input" type="text" id="firstname" onChange={e =>this.handleFirstNameChange(e)} value={this.state.user.birthDatefirstName} />
                     <label htmlFor="lastname">Lastname</label>
-                    <input className="uk-input" type="text" id="lastname" onChange={e =>this.handleLastNameChange(e)} value={this.state.lastName} />
+                    <input className="uk-input" type="text" id="lastname" onChange={e =>this.handleLastNameChange(e)} value={this.state.user.lastName} />
+
+                    <label htmlFor="countries-select">Country</label>
+                    <select
+                        name="countries"
+                        id="countries-select"
+                        className="uk-select"
+                        value={this.state.user.country}
+                        onChange={e =>this.handleCountryChange(e)}
+                    >
+                        <option value="">--Please choose a country--</option>
+                        {this.state.countries.map(country => (
+                            <option key={country.country_id} value={country.country_id}>{country.country}</option>
+					    ))}
+                    </select>
+
                     <label htmlFor="username">Username</label>
-                    <input className="uk-input" type="text" id="username" onChange={e =>this.handleUserNameChange(e)} value={this.state.username} />
+                    <input className="uk-input" type="text" id="username" onChange={e =>this.handleUserNameChange(e)} value={this.state.user.username} />
                     <label htmlFor="email">E-mail</label>
-                    <input className="uk-input" type="email" id="email" onChange={e =>this.handleEmailChange(e)} value={this.state.email} />
+                    <input className="uk-input" type="email" id="email" onChange={e =>this.handleEmailChange(e)} value={this.state.user.email} />
                     <label htmlFor="description">Description</label>
                     <textarea
                         id="description"
                         rows="4"
                         onChange={e =>this.handleDescriptionChange(e)}
-                        value={this.state.description}
+                        value={this.state.user.description}
                         className="uk-textarea"
 				    ></textarea>
                     <label htmlFor="birthdate">Birthdate</label>
-                    <input className="uk-input" type="date" id="birthdate" onChange={e =>this.handleBirthDateChange(e)} value={this.state.birthDate}  />
+                    <input className="uk-input" type="date" id="birthdate" onChange={e =>this.handleBirthDateChange(e)} value={this.state.user.birthDate}  />
                     <label htmlFor="password">Password</label>
-                    <input className="uk-input" type="password" id="password" onChange={e =>this.handlePasswordChange(e)} value={this.state.password} />
+                    <input className="uk-input" type="password" id="password" onChange={e =>this.handlePasswordChange(e)} value={this.state.user.password} />
                     <label htmlFor="repassword">Re-password</label>
-                    <input className="uk-input" type="password" id="repassword" onChange={e =>this.handleRePasswordChange(e)} value={this.state.repassword} />
+                    <input className="uk-input" type="password" id="repassword" onChange={e =>this.handleRePasswordChange(e)} value={this.state.user.repassword} />
                     <button className="uk-button uk-button-primary uk-width-1-1" type="submit">Envoyer</button>
                 </form>
 			</div>
