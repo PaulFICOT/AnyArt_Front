0b5f8153e22a88d268bc30d64064fcac3d5de572
justin <justin.mottier@gmail.com>
2021-05-11 18:04:26 +0200

    Logo style

---
+++ src/js/Logo.js
@@ -0,0 +1,14 @@
+import 'src/css/Logo.css';
+
+export default function Logo() {
+	return (
+		<>
+			<span className="logo-any">
+				<span className="logo-first-letter">A</span>NY
+			</span>
+			<span className="logo-art">
+				<span className="logo-first-letter">A</span>rt
+			</span>
+		</>
+	);
+}
