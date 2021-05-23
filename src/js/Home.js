a68cf8f3dd83305d79377dd9a1c180b2a98a648e
Paul FICOT <paul.ficot@gmail.com>
2021-05-24 00:45:02 +0200

    WiP Filter

--- src/js/Home.js
+++ src/js/Home.js
@@ -13,6 +13,10 @@ export default function Home(props) {
 	const [posts, setPosts] = useState([]);
 	const [categories, setCategories] = useState([]);
 
+	const [xd, setxd] = useState({
+		OpenToWork: false,
+	});
+	
 	function getUserId() {
 		if(AuthService.getCurrentUser()) {
 			return AuthService.getCurrentUser().user_id;
@@ -40,9 +44,13 @@ export default function Home(props) {
 
 	}
 
-	function filter() {
-		console.log('xd');
+	function setToggle(id, isToggled) {
+		let v = xd;
+		v[id] = isToggled;
+		setxd(v);
 	}
+
+	console.log(xd)
 
 	let isLogged = useContext(AuthContext).isLogin;
 
@@ -61,20 +69,14 @@ export default function Home(props) {
 				<div><button className={"uk-button uk-button-link"} onClick={() => PostRequests.getThumbnailsUnlogged('hottest').then(data => setPosts(data))} style={{textDecoration: 'none'}}><span><i className="fas fa-fire"></i> Hottest</span></button></div>
 				<div><button className={"uk-button uk-button-link"} onClick={() => PostRequests.getThumbnailsUnlogged('raising').then(data => setPosts(data))} style={{textDecoration: 'none'}}><span><i className="fas fa-chart-line"></i> Raising</span></button></div>
 				<div>
-					<button className={"uk-button uk-button-link"} onClick={() => fetchCategories()} style={{textDecoration: 'none'}}><span className="uk-flex-right"><i className="fas fa-angle-down"></i> Filter</span></button>
+					<button className={"uk-button uk-button-link"} onClick={() => fetchCategories()} style={{textDecoration: 'none'}}><span className="uk-flex-right"><i className="fas fa-angle-down"/>Filter</span></button>
 					<div data-uk-dropdown="mode:click">
 						<ul className="uk-nav uk-dropdown-nav">
-							<li class="uk-nav-header">Job</li>
-							<li>
-								<div className=".uk-flex-right">
-									OpenToWork<Toggle/>
-								</div>
-							</li>
-							<li class="uk-nav-header">Categories</li>
+							<li className="uk-nav-header">Job</li>
+								<Toggle id="OpenToWork" text="OpenToWork" xd={setToggle}/>
+							<li className="uk-nav-header">Categories</li>
 							{categories.map(category => (
-								<li key={category.category_id}>
-									{category.category}
-								</li>
+								<Toggle key={category.category_id} id={category.category_id} text={category.category} xd={setxd}/>
 							))}
 						</ul>
 					</div>
@@ -83,7 +85,9 @@ export default function Home(props) {
 			</div>
 			<div className="uk-grid-row-medium uk-child-width-1-5" data-uk-grid>
 				{posts.map(post => (
-					<div key={post.post_id}><Thumbnail src={post.url} /></div>
+					<div key={post.post_id}>
+						<Thumbnail src={post.url} />
+					</div>
 				))}
 			</div>
 		</Page>
--- src/js/Toggle.js
+++ src/js/Toggle.js
@@ -2,12 +2,20 @@ import 'src/css/toggle.css';
 
 export default function Toggle(props) {
 
+    function onChecked(event) {
+        console.log(event.target.checked);
+        props.xd("OpenToWork", event.target.checked);
+    }
+
     return(
+        <span>
+			<div className="uk-item-text">{props.text}</div>						
         <div>
-            <label className="uk-switch" for="default-1">
-                <input type="checkbox" id="default-1"/>
-                <div class="uk-switch-slider"></div>
+            <label className="uk-switch" htmlFor={props.id}>
+                <input type="checkbox" id={props.id} onChange={(event) => onChecked(event)}/>
+                <div className="uk-switch-slider"></div>
             </label>
         </div>
+        </span>
     );
 }
