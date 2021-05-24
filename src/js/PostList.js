0f18acb3b10a57f40f41ede039382ce4ea861be5
justin <justin.mottier@gmail.com>
2021-05-24 17:25:10 +0200

    correct thumb + link

--- src/js/PostList.js
+++ src/js/PostList.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Thumbnail from './Component/Thumbnail';
+import HttpClient from './HttpRequests/HttpClient';
+import { Link } from 'react-router-dom';
 
 export default function PostList(props) {
-
 	return (
-        <div className="uk-grid-row-medium uk-child-width-1-5" data-uk-grid>
-            {props.posts.map(post => (
-                <div key={post.post_id}><Thumbnail src={post.url} /></div>
-            ))}
-        </div>
+		<div className="uk-grid-row-medium uk-child-width-1-5" data-uk-grid>
+			{props.posts.map(post => (
+				<div key={post.post_id}>
+					<Link to={`/post/${post.post_id}`}>
+						<Thumbnail src={HttpClient.imageUrl(post.picture_id)} />
+					</Link>
+				</div>
+			))}
+		</div>
 	);
 }
