bf556fd49b96b4dee855188275434c500e1d8324
Romain Dubuc <romaindubuc62@gmail.com>
2021-05-24 22:32:28 +0200

    Add comments for CategoriesRequests/CountriesRequests/HttpClient/ImageRequests/PostRequests

--- src/js/HttpRequests/CategoriesRequests.js
+++ src/js/HttpRequests/CategoriesRequests.js
@@ -2,7 +2,13 @@ import HttpClient from './HttpClient';
 
 const httpClient = new HttpClient();
 
+/**
+ * Class that allows to get categories information
+ */
 export default class CategoriesRequests {
+	/**
+	 * Get all categories
+	 */
 	static getAll() {
 		return httpClient.get('categories');
 	}
--- src/js/HttpRequests/CountriesRequests.js
+++ src/js/HttpRequests/CountriesRequests.js
@@ -2,7 +2,13 @@ import HttpClient from './HttpClient';
 
 const httpClient = new HttpClient();
 
+/**
+ * Class that allows to get countries information
+ */
 export default class CountriesRequest {
+	/**
+	 * Get all countries
+	 */
 	static getAll() {
 		return httpClient.get('countries');
 	}
